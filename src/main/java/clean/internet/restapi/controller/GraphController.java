fd16b87f2d61effa7e59012b21a3e78dd94aa45e
Inpyo-Jeon <inpyo.developer@gmail.com>
2019-04-20 16:32:45 +0900

    Add notify slack about exception at controller

--- src/main/java/clean/internet/restapi/controller/GraphController.java
+++ src/main/java/clean/internet/restapi/controller/GraphController.java
@@ -1,42 +1,69 @@
 package clean.internet.restapi.controller;
 
+import ch.qos.logback.classic.Logger;
 import clean.internet.restapi.common.LineGraphType;
 import clean.internet.restapi.service.GraphService;
+import clean.internet.restapi.util.NotifierSlack;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.text.ParseException;
 
 @RestController
 public class GraphController {
 
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(GraphController.class);
+
     @Autowired
     GraphService graphService;
 
+    @Autowired
+    NotifierSlack notifierSlack;
+
     @RequestMapping(value = "/data/graph/line")
-    public String lineGraph(
+    public ResponseEntity lineGraph(
             @RequestParam(value = "type") String type
             , @RequestParam(value = "begin") String begin
-            , @RequestParam(value = "end") String end) throws IOException, ParseException {
+            , @RequestParam(value = "end") String end) throws JsonProcessingException {
 
-        LineGraphType graphType = LineGraphType.find(type);
-        String data = "";
+        ResponseEntity responseEntity;
 
-        switch (graphType) {
-            case DAY:
-                data = graphService.getLineGraphDataForDay(begin, end);
-                break;
-            case MONTH:
-                data = graphService.getLineGraphDataForMonth(begin, end);
-                break;
-            case YEAR:
-                data = graphService.getLineGraphDataForYear(begin, end);
-                break;
+        try {
+
+            LineGraphType graphType = LineGraphType.find(type);
+            String data = "";
+
+            switch (graphType) {
+                case DAY:
+                    data = graphService.getLineGraphDataForDay(begin, end);
+                    break;
+                case MONTH:
+                    data = graphService.getLineGraphDataForMonth(begin, end);
+                    break;
+                case YEAR:
+                    data = graphService.getLineGraphDataForYear(begin, end);
+                    break;
+            }
+
+            responseEntity = ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(data);
+        } catch (Exception exception) {
+
+            responseEntity = ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Sorry, Graph Line Error");
+
+            logger.error("error : ", exception);
+            notifierSlack.notifySend(ExceptionUtils.getRootCauseMessage(exception), ExceptionUtils.getStackTrace(exception));
         }
 
-        return data;
+        return responseEntity;
     }
 }
---
+++ src/main/java/clean/internet/restapi/util/NotifierSlack.java
@@ -0,0 +1,150 @@
+package clean.internet.restapi.util;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class NotifierSlack {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @Autowired
+    HttpHeaders httpHeaders;
+
+    @Value("${common.notification.exception.webhook-url}")
+    String webHookUrl;
+
+    @Value("${common.notification.exception.channel-name}")
+    String channel;
+
+    @Value("${common.notification.exception.color}")
+    String color;
+
+    @Value("${spring.profiles.active}")
+    String profile;
+
+    public class basicData {
+        String username;
+        String text;
+
+        public basicData(String username, String text) {
+            this.username = username;
+            this.text = text;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+    }
+
+    public class attachmentData {
+        @JsonProperty(value = "username")
+        private String userName;
+
+        public String getUsername() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+
+        @JsonProperty(value = "attachments")
+        List<InnerData> attachments = new ArrayList<>();
+
+        public InnerData createInnerData() {
+            return new InnerData();
+        }
+
+        public class InnerData {
+            @JsonProperty(value = "color")
+            private String color;
+            @JsonProperty(value = "pretext")
+            private String preText;
+            @JsonProperty(value = "text")
+            private String text;
+            @JsonProperty(value = "ts")
+            private long ts;
+
+            public String getColor() {
+                return color;
+            }
+
+            public void setColor(String color) {
+                this.color = color;
+            }
+
+            public String getPreText() {
+                return preText;
+            }
+
+            public void setPreText(String preText) {
+                this.preText = preText;
+            }
+
+            public String getText() {
+                return text;
+            }
+
+            public void setText(String text) {
+                this.text = text;
+            }
+
+            public long getTs() {
+                return ts;
+            }
+
+            public void setTs(long ts) {
+                this.ts = ts;
+            }
+        }
+    }
+
+    public void notifySend(String exception, String stackTrace) throws JsonProcessingException {
+        attachmentData attachmentData = new attachmentData();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        NotifierSlack.attachmentData.InnerData innerData = attachmentData.createInnerData();
+        innerData.setColor(color);
+        innerData.setPreText(exception);
+        innerData.setText(stackTrace);
+        innerData.setTs(new Date().toInstant().getEpochSecond());
+
+        attachmentData.attachments.add(innerData);
+        attachmentData.setUserName("[" + profile.toUpperCase() + "] - RestAPI");
+
+        String sendData = mapper.writeValueAsString(attachmentData);
+
+        HttpEntity param = new HttpEntity(sendData, httpHeaders);
+
+        restTemplate.postForObject(webHookUrl, param, String.class);
+    }
+}
